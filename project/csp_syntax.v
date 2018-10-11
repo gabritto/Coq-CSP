c503241bc6821bf69eaf3755a4baa19f99d44a1a
gab <gab@cin.ufpe.br>
2018-10-11 09:20:13 -0300

    new notes

--- project/csp_syntax.v
+++ project/csp_syntax.v
@@ -1,5 +1,6 @@
 Require Import Coq.Lists.ListSet.
 Require Import Coq.Lists.List.
+Import ListNotations.
 Module CSP_Syntax.
 
 (* Define Event type *)
@@ -54,13 +55,14 @@ Definition setMap := @set_map Trace Trace Trace_eq_dec.
 
 Fixpoint traces (p: Proc): set Trace :=
   match p with
-  | Stop => sngEmptyTrace
+  | Stop => [emptyTrace]
   | ProcPref e q =>
     let qTraces := traces(q) in
-    let qWithA := setMap (fun trace => e :: trace) qTraces in setUnion sngEmptyTrace qWithA
+    let qWithA := setMap (fun trace => e :: trace) qTraces in
+      setUnion [emptyTrace] qWithA
   | ProcExtChoice p q => setUnion (traces p) (traces q)
   | ProcCond b p q => if b then (traces p) else (traces q)
-  | _ => sngEmptyTrace
+  | _ => [emptyTrace]
   end.    
 
 Lemma set_union_not_empty: forall {T: Type}
@@ -68,21 +70,24 @@ Lemma set_union_not_empty: forall {T: Type}
   (s t: set T),
   s <> empty_set T -> set_union T_eq_dec s t <> empty_set T.
 Proof.
-Admitted.
+  intros T eq_dec s t.
+  destruct t as [ |b t'].
+  - unfold empty_set. intros H. simpl. apply H.
+  - intros H. simpl. apply set_add_not_empty.
+Qed.
 
-Lemma sngTrace_not_empty: forall (t: Trace), sngTrace t <> empty_set Trace.
+Lemma sngTrace_not_empty: forall (t: Trace), [t] <> empty_set Trace.
 Proof.
   intros t.
   unfold sngTrace.
-  apply set_add_not_empty.
+  unfold empty_set. intros H. inversion H.
 Qed.
 
 Theorem traces_non_empty: forall (p: Proc),
   traces p <> empty_set Trace.
 Proof.
   intros p. induction p.
-  - simpl. unfold sngEmptyTrace. unfold sngTrace.
-    apply set_add_not_empty.
+  - simpl. unfold empty_set. intros H. inversion H.
   - simpl. unfold setUnion. simpl. Search (_ <> empty_set).
     apply set_union_not_empty. apply sngTrace_not_empty.
   - simpl. apply set_union_not_empty. apply IHp1.
@@ -91,6 +96,14 @@ Proof.
     + apply IHp2.
   - simpl. apply sngTrace_not_empty. 
 Qed.
+
+Fixpoint prefixes {T: Type} (s: list T): list (list T) :=
+  match s with
+  | nil => [nil]
+  | a :: s' =>
+    let ps' := prefixes s' in
+    nil :: map (fun p => a :: p) ps'
+  end.
 
 (*
 Definir prop prefixClosed e provar teorema abaixo.
